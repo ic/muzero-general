7faf220ffbd1bb72fb1176b74887c72ba4545bd8
Eric Platon <zaraki@gmx.com>
2022-01-05 15:16:40 +0900

    Tentative setuptools wrapping.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="muzero",
+    version="1.0",
+    author="Werner Duvaud et al",
+    zip_safe=False,
+    install_requires=[
+        "gym",
+        "hiredis",
+        "nevergrad",
+        "numpy",
+        "ray",
+        "torch==1.7.1"
+    ]
+)
